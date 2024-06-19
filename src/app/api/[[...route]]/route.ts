1f97dd3cc8921c944dff95167b0c0aaf257a352f
ndaden <nabil.daden@gmail.com>
2024-06-19 23:24:25 +0200

    nextjs + elysia

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,32 @@
+import bearer from "@elysiajs/bearer";
+import { Elysia, t } from "elysia";
+
+const app = new Elysia({ prefix: "/api" })
+  .get("/", () => "Hello from Elysia")
+  .use(bearer())
+  .onBeforeHandle(async ({ bearer, set }) => {
+    if (!bearer) return (set.status = "Unauthorized");
+    const isAuthorized = bearer === "12345678";
+    if (!isAuthorized) {
+      return (set.status = "Unauthorized");
+    }
+  })
+  .post(
+    "/sign",
+    ({ bearer, body }) => {
+      return body;
+    },
+    {
+      body: t.Object({
+        name: t.String(),
+        password: t.String(),
+      }),
+    }
+  );
+
+export type App = typeof app;
+
+export const GET = app.handle;
+export const POST = app.handle;
+export const PUT = app.handle;
+export const DELETE = app.handle;

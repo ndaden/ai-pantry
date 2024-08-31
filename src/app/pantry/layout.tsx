dc803af795fce7fcf39fd12c5fa2f21ba7e00fb0
ndaden <nabil.daden@gmail.com>
2024-08-31 20:40:38 +0200

    fix kinde auth

--- src/app/pantry/layout.tsx
+++ src/app/pantry/layout.tsx
@@ -1,14 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <KindeProvider>
-      <MaxWidthWrapper className="flex-1 flex flex-col">
-        {children}
-      </MaxWidthWrapper>
-    </KindeProvider>
+    <MaxWidthWrapper className="flex-1 flex flex-col">
+      {children}
+    </MaxWidthWrapper>
   );
 };
 

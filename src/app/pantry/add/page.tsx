db695dad504462a60237dab4dbaf3b4718d896fa
ndaden <nabil.daden@gmail.com>
2024-08-31 20:44:11 +0200

    add kindeProvider

--- src/app/pantry/add/page.tsx
+++ src/app/pantry/add/page.tsx
@@ -27,7 +27,7 @@ import { Product } from "@/lib/types/Product";
 import { useState } from "react";
 import { QUANTITIES, UNITS } from "./constants";
 import useProductController from "@/lib/hooks/useProductController";
-import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import { KindeProvider, useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 
 const pantryFormSchema = z.object({
   userId: z.string(),
@@ -84,108 +84,46 @@ const PantryForm = () => {
     addProductMutation({ ...product, userId: user?.id || "" });
   };
 
-  return isLoadingCategories ? (
-    <div className="flex-1 flex flex-col items-center justify-center">
-      <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
-    </div>
-  ) : (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="label"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Libellé</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Tomate"
-                  className="h-12 text-lg"
-                  {...field}
-                />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="categoryId"
-          render={({ field }) => (
-            <RadioGroup {...field}>
-              <FormLabel className="">Catégorie</FormLabel>
-              <Field className="flex flex-wrap justify-center gap-3 mt-5">
-                {(categories?.data || []).map((categorie) => {
-                  return (
-                    <Radio
-                      key={categorie.id}
-                      value={categorie.id}
-                      className={({ checked, hover }) =>
-                        cn(
-                          "flex items-center transition relative p-3 w-fit shadow-xl rounded-2xl border border-zinc-400",
-                          { "border-black": hover },
-                          { "bg-green-600 text-white border-0": checked }
-                        )
-                      }
-                    >
-                      <span className="mr-2">
-                        {
-                          CATEGORY_ICON.find((c) => c.name === categorie.label)
-                            ?.icon
-                        }
-                      </span>
-                      <span className="text-sm">{categorie.label}</span>
-                    </Radio>
-                  );
-                })}
-              </Field>
-            </RadioGroup>
-          )}
-        ></FormField>
-
-        <FormField
-          control={form.control}
-          name="quantityUnit"
-          render={({ field }) => (
-            <RadioGroup {...field}>
-              <FormLabel>Quantité</FormLabel>
-              <Field className="flex flex-wrap justify-center gap-3 mt-5">
-                {UNITS.map((unit) => {
-                  return (
-                    <Radio
-                      key={unit.name}
-                      value={unit.value}
-                      className={({ checked, hover }) =>
-                        cn(
-                          "flex items-center transition relative p-3 w-fit shadow-xl rounded-2xl border border-zinc-400",
-                          { "border-black": hover },
-                          { "bg-green-600 text-white border-0": checked }
-                        )
-                      }
-                    >
-                      <span className="text-sm">{unit.name}</span>
-                    </Radio>
-                  );
-                })}
-              </Field>
-            </RadioGroup>
-          )}
-        ></FormField>
-        <div className="flex justify-center items-baseline">
-          {!moreQuantity && (
+  return (
+    <KindeProvider>
+      {isLoadingCategories ? (
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+        </div>
+      ) : (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
               control={form.control}
-              name="quantity"
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Libellé</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Tomate"
+                      className="h-12 text-lg"
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="categoryId"
               render={({ field }) => (
                 <RadioGroup {...field}>
+                  <FormLabel className="">Catégorie</FormLabel>
                   <Field className="flex flex-wrap justify-center gap-3 mt-5">
-                    {QUANTITIES.map((quantity) => {
+                    {(categories?.data || []).map((categorie) => {
                       return (
                         <Radio
-                          key={quantity.value}
-                          value={quantity.value}
+                          key={categorie.id}
+                          value={categorie.id}
                           className={({ checked, hover }) =>
                             cn(
                               "flex items-center transition relative p-3 w-fit shadow-xl rounded-2xl border border-zinc-400",
@@ -194,7 +132,14 @@ const PantryForm = () => {
                             )
                           }
                         >
-                          <span className="text-sm">{quantity.name}</span>
+                          <span className="mr-2">
+                            {
+                              CATEGORY_ICON.find(
+                                (c) => c.name === categorie.label
+                              )?.icon
+                            }
+                          </span>
+                          <span className="text-sm">{categorie.label}</span>
                         </Radio>
                       );
                     })}
@@ -202,46 +147,111 @@ const PantryForm = () => {
                 </RadioGroup>
               )}
             ></FormField>
-          )}
-          {moreQuantity && (
+
             <FormField
               control={form.control}
-              name="quantity"
+              name="quantityUnit"
               render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="Saisir la quantité"
-                      className="h-10 text-lg shadow-lg border-zinc-400 w-14 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                      type="number"
-                      onChange={(e) => field.onChange(e.target.valueAsNumber)}
-                      onBlur={field.onBlur}
-                      value={Number.parseInt(`${field.value}`)}
-                    />
-                  </FormControl>
-
-                  <FormMessage />
-                </FormItem>
+                <RadioGroup {...field}>
+                  <FormLabel>Quantité</FormLabel>
+                  <Field className="flex flex-wrap justify-center gap-3 mt-5">
+                    {UNITS.map((unit) => {
+                      return (
+                        <Radio
+                          key={unit.name}
+                          value={unit.value}
+                          className={({ checked, hover }) =>
+                            cn(
+                              "flex items-center transition relative p-3 w-fit shadow-xl rounded-2xl border border-zinc-400",
+                              { "border-black": hover },
+                              { "bg-green-600 text-white border-0": checked }
+                            )
+                          }
+                        >
+                          <span className="text-sm">{unit.name}</span>
+                        </Radio>
+                      );
+                    })}
+                  </Field>
+                </RadioGroup>
               )}
-            />
-          )}
-          <Button
-            type="button"
-            onClick={() => {
-              setMoreQuantity((prev) => !prev);
-            }}
-            variant={"outline"}
-            className="ml-3 h-13 py-3 border rounded-2xl border-zinc-400 shadow-lg"
-          >
-            {moreQuantity ? "x" : "Plus"}
-          </Button>
-        </div>
+            ></FormField>
+            <div className="flex justify-center items-baseline">
+              {!moreQuantity && (
+                <FormField
+                  control={form.control}
+                  name="quantity"
+                  render={({ field }) => (
+                    <RadioGroup {...field}>
+                      <Field className="flex flex-wrap justify-center gap-3 mt-5">
+                        {QUANTITIES.map((quantity) => {
+                          return (
+                            <Radio
+                              key={quantity.value}
+                              value={quantity.value}
+                              className={({ checked, hover }) =>
+                                cn(
+                                  "flex items-center transition relative p-3 w-fit shadow-xl rounded-2xl border border-zinc-400",
+                                  { "border-black": hover },
+                                  {
+                                    "bg-green-600 text-white border-0": checked,
+                                  }
+                                )
+                              }
+                            >
+                              <span className="text-sm">{quantity.name}</span>
+                            </Radio>
+                          );
+                        })}
+                      </Field>
+                    </RadioGroup>
+                  )}
+                ></FormField>
+              )}
+              {moreQuantity && (
+                <FormField
+                  control={form.control}
+                  name="quantity"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input
+                          placeholder="Saisir la quantité"
+                          className="h-10 text-lg shadow-lg border-zinc-400 w-14 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                          type="number"
+                          onChange={(e) =>
+                            field.onChange(e.target.valueAsNumber)
+                          }
+                          onBlur={field.onBlur}
+                          value={Number.parseInt(`${field.value}`)}
+                        />
+                      </FormControl>
 
-        <Button type="submit" className="sm:w-fit w-full" size={"lg"}>
-          Ajouter
-        </Button>
-      </form>
-    </Form>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+              <Button
+                type="button"
+                onClick={() => {
+                  setMoreQuantity((prev) => !prev);
+                }}
+                variant={"outline"}
+                className="ml-3 h-13 py-3 border rounded-2xl border-zinc-400 shadow-lg"
+              >
+                {moreQuantity ? "x" : "Plus"}
+              </Button>
+            </div>
+
+            <Button type="submit" className="sm:w-fit w-full" size={"lg"}>
+              Ajouter
+            </Button>
+          </form>
+        </Form>
+      )}
+      )
+    </KindeProvider>
   );
 };
 

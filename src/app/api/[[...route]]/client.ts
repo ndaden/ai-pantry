4f44eb9223809e79b23f7302331cac07f34647c3
ndaden <nabil.daden@gmail.com>
2024-09-02 22:50:07 +0200

    fix cors

--- src/app/api/[[...route]]/client.ts
+++ src/app/api/[[...route]]/client.ts
@@ -2,5 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import { App } from "./route";
 
 export const client = treaty<App>(
-  process.env.KINDE_SITE_URL || "https://ai-pantry-rho.vercel.app/"
+  process.env.KINDE_SITE_URL || "https://aipantry.dnabil.ovh/"
 );
